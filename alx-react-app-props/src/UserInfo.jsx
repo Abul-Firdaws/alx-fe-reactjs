f175a130838c01ca5b788f5ac8cd7ef60c223a00
Saaka Abdul-Firdaws <saakaabufirdaus@gmail.com>
2025-09-07 17:04:22 +0000

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import UserProfile from './UserProfile';
+import React from "react";
+import UserProfile from "./UserProfile";
 
 function UserInfo() {
   return <UserProfile />;

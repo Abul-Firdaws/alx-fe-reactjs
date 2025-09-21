1939a0981b000053c1d829399245e509ea52a214
Saaka Abdul-Firdaws <saakaabufirdaus@gmail.com>
2025-09-21 16:14:51 +0000

    polish: remove all decorative icons for ultra-minimal design

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -188,12 +188,7 @@ function Search() {
                       Searching...
                     </>
                   ) : (
-                    <>
-                      <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                      </svg>
-                      Search User
-                    </>
+                    "Search User"
                   )}
                 </button>
                 
@@ -277,12 +272,7 @@ function Search() {
                       Searching...
                     </>
                   ) : (
-                    <>
-                      <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 100 4m0-4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 100 4m0-4v2m0-6V4" />
-                      </svg>
-                      Advanced Search
-                    </>
+                    "Advanced Search"
                   )}
                 </button>
                 <button
